f91ee95ca76534245f86de3646147d3bec06f8fd
alireza_amini <aminicss1@gmail.com>
2023-03-27 20:07:51 +0330

    refactor: put meta tags in document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,10 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content="React Search Component" />
+      </Head>
       <body>
-        <Main />
-        <NextScript />
+      <Main />
+      <NextScript />
       </body>
     </Html>
   )
