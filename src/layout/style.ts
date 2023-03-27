095a926ec69b4b0873d95e6b474eb04ee902d8c8
alireza_amini <aminicss1@gmail.com>
2023-03-27 19:13:02 +0330

    feat: add layout
    Added:
    - color pallet

---
+++ src/layout/style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import {colors} from "@/styles/colors";
+
+export const LayoutContainer = styled.div`
+  position: relative;
+  width: 500px;
+  max-width: 100%;
+  height: calc(100vh - 128px);
+  overflow: auto;
+  background: ${colors.white};
+  border: 1px solid ${colors.secondary};
+  margin: 0 auto;
+  transition: all 200ms;
+`;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import type {AppProps} from 'next/app'
 import GlobalStyles from "@/styles/global-styles";
+import {Layout} from "@/layout";
 
 export default function App({Component, pageProps}: AppProps) {
   return (
     <>
       <GlobalStyles />
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </>
   )
 }
