46e4b247521e26c1eb719dcf4f58bd7bea1cad56
alireza_amini <aminicss1@gmail.com>
2023-03-28 14:39:43 +0330

    refactor: improve clear icon

--- src/components/icons/ClearIcon.tsx
+++ src/components/icons/ClearIcon.tsx
@@ -2,8 +2,10 @@ import * as React from 'react';
 import {SVGProps} from 'react';
 
 const ClearIcon = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" {...props}>
-    <path d="M3.707 3.293a1 1 0 0 1 1.414 0L12 10.586l6.879-6.879a1 1 0 1 1 1.414 1.414L13.414 12l6.879 6.879a1 1 0 0 1-1.414 1.414L12 13.414l-6.879 6.879a1 1 0 0 1-1.414-1.414L10.586 12 3.707 5.121a1 1 0 0 1 0-1.414z" fill="currentColor"/>
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <circle cx="12" cy="12" r="9" stroke="currentColor"/>
+    <path d="M9.00009 14.9997L15.0001 8.99966" stroke="currentColor"/>
+    <path d="M15 15L9 9" stroke="currentColor"/>
   </svg>
 );
 
