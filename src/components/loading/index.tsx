4b55479325058c4e1ade4e33d02d3d2f765fa399
alireza_amini <aminicss1@gmail.com>
2023-03-28 13:03:57 +0330

    feat: add router loading

---
+++ src/components/loading/index.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import {LoadingStyle} from './style';
+
+export const Loading = () => {
+  return (
+    <LoadingStyle>
+      <div className="container">
+        <div className="dot dot-1"></div>
+        <div className="dot dot-2"></div>
+        <div className="dot dot-3"></div>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+        <defs>
+          <filter id="goo">
+            <feGaussianBlur
+              in="SourceGraphic"
+              stdDeviation="10"
+              result="blur"
+            />
+            <feColorMatrix
+              in="blur"
+              mode="matrix"
+              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 21 -7"
+            />
+          </filter>
+        </defs>
+      </svg>
+      <div className="overlay" />
+    </LoadingStyle>
+  );
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,30 @@
 import type {AppProps} from 'next/app'
 import GlobalStyles from "@/styles/global-styles";
 import {Layout} from "@/layout";
+import {useEffect} from "react";
+import {useRouter} from "next/router";
+import {useBoolean} from "@/hooks/useBoolean";
+import {Loading} from "@/components/loading";
 
 export default function App({Component, pageProps}: AppProps) {
+  const router = useRouter();
+  const [loading, setLoading] = useBoolean(false);
+
+  useEffect(() => {
+    const handleStart = (url: string) => {
+      url !== router.pathname ? setLoading.setTrue() : setLoading.setFalse();
+    };
+    const handleComplete = () => setLoading.setFalse();
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+  }, [router, setLoading]);
   return (
     <>
       <GlobalStyles />
       <Layout>
+        {loading && <Loading />}
         <Component {...pageProps} />
       </Layout>
     </>
